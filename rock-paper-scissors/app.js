f60ec7bdc7432b5de153fb90a5f11f13b442b1f2
Jeremiah Adsit <jeremiah.adsit@gmail.com>
2022-03-13 15:39:04 -0600

    RPS: added basic computer choice randomizer and display

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,10 +1,26 @@
-const computerChoice = document.getElementById('computer-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
 const playerChoiceDisplay = document.getElementById('player-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
+let computerChoice;
 let playerChoice;
 
 possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
     playerChoice = e.target.id;
     playerChoiceDisplay.innerHTML = playerChoice;
+    generateComputerChoice();
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length);
+
+    if (randomNumber === 0) {
+        computerChoice = 'rock';
+    } else if (randomNumber === 1) {
+        computerChoice = 'paper';
+    } else if (randomNumber === 2) {
+        computerChoice = 'scissors';
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
