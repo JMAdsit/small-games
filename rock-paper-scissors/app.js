0230094149269ac8ecf644101fa586f6e04430ab
Jeremiah Adsit <jeremiah.adsit@gmail.com>
2022-03-14 13:10:17 -0600

    RPS: added result function and rendering

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let computerChoice;
 let playerChoice;
+let result;
 
 possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
     playerChoice = e.target.id;
     playerChoiceDisplay.innerHTML = playerChoice;
     generateComputerChoice();
+    getResult();
 }))
 
 function generateComputerChoice() {
@@ -23,4 +25,33 @@ function generateComputerChoice() {
     }
 
     computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+    if (computerChoice === playerChoice) {
+        result = "It's a draw!";
+    } 
+
+    if (computerChoice === "rock" && playerChoice === "paper") {
+        result = "You win!"
+    }
+    if (computerChoice === "rock" && playerChoice === "scissors") {
+        result = "You lose!"
+    }
+
+    if (computerChoice === "paper" && playerChoice === "scissors") {
+        result = "You win!"
+    }
+    if (computerChoice === "paper" && playerChoice === "rock") {
+        result = "You lose!"
+    }
+
+    if (computerChoice === "scissors" && playerChoice === "rock") {
+        result = "You win!"
+    }
+    if (computerChoice === "scissors" && playerChoice === "paper") {
+        result = "You lose!"
+    }
+
+    resultDisplay.innerHTML = result;
 }
